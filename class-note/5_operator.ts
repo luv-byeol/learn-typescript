ecb7bd6892bddc6ea008f4e73ba38eddca9cff6a
이소현 <luv_byeol@icloud.com>
2022-04-07 17:15:37 +0900

    220407

---
+++ class-note/5_operator.ts
@@ -0,0 +1,43 @@
+// function logMessage(value: string) {
+//   console.log(value);
+// }
+// logMessage('hello');
+
+var seho: string | number | boolean;
+function logMessage(value: string | number) {
+  if(typeof value ==='number') {
+    value.toLocaleString()
+  }
+  if(typeof value ==='string') {
+    value.toString();
+  }
+  throw new TypeError('value must be string or number')
+}
+logMessage('hello');
+logMessage(100);
+
+interface Developer {
+  name: string;
+  skill: string;
+
+}
+
+interface Person {
+  name: string,
+  age: number;
+}
+
+// function askSomeone(someone: Developer | Person) {
+//   someone.name;
+//   someone.skill;
+//   someone.age;
+// }
+
+function askSomeone(someone: Developer & Person) {
+  // someone.name;
+  // someone.skill;
+  // someone.age;
+}
+
+askSomeone({name: '디벨로퍼', skill: '웹 개발', age: 34});
+// askSomeone({name: '캡틴', age: 100})
---
+++ class-note/7_class.ts
@@ -0,0 +1,13 @@
+class Person {
+  private name: string;
+  public age: number;
+  readonly log: string;
+  // 타입 먼저 지정 해주어야 함
+  // 변수의 유효범위 이 변수 안에서만 사용 할 때 private
+  // 기본적인 것 public
+
+  constructor(name: string, age:number) {
+    this.name = name;
+    this.age = age;
+  }
+}
